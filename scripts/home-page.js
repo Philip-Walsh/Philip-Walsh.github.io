f3b64303e221ec4ca2fd3be4226c0f097a368167
Philip Walsh <59885325+Philip-Walsh@users.noreply.github.com>
2023-11-18 14:50:19 +0000

    update header/footer

--- scripts/home-page.js
+++ scripts/home-page.js
@@ -1,6 +1,6 @@
 var headerHtml = `
 <a id="logo" class="flex">
-    <h1>Portfolio Site</h1>
+    <h1>Portfolio Coming Soon!</h1>
 </a>
 `
 var flex = "flex"
@@ -15,37 +15,59 @@ function getImage(src, alt) {
     return img;
 }
 
+HTMLElement.prototype.prependSvg = function (path, ariaLabel, eventType, eventCallback) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', `/assets/${path}.svg`, true);
+    xhr.responseType = 'document';
+
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            let svgElement = xhr.response.documentElement;
+            svgElement.setAttribute('aria-label', ariaLabel);
+            this.prepend(svgElement);
+
+            if (eventType && typeof eventCallback === 'function') {
+                svgElement.addEventListener(eventType, eventCallback);
+            }
+        } else {
+            console.error('Error loading SVG:', xhr.statusText);
+        }
+    };
+    xhr.send();
+}
+
 function addHeader() {
     let header = document.createElement("header");
     header.innerHTML = headerHtml;
     header.className = flex
     document.body.prepend(header);
-    logoElem = document.querySelector('#logo')
-    logoElem.prepend(getImage('favicon.svg', 'Logo Images'));
-    logoElem.addEventListener('click', function (event) {
-        event.preventDefault();
-        window.location.href = '/';
-    });
+
+    let logoElem = document.querySelector('#logo');
+    if (logoElem) {
+        logoElem.prependSvg('favicon', 'Logo Image');
+        logoElem.addEventListener('click', function (event) {
+            event.preventDefault();
+            window.location.href = '/';
+        });
+    }
 }
 
 function addFooter() {
     let footer = document.createElement("footer");
-    footer.innerHTML = "<p>&copy; <i>Philip Walsh 2023</i></p>";
+    footer.innerHTML = "<section></section><section>&copy; <i>Philip Walsh 2023</i></section>";
     footer.className = flex;
 
     let socials = document.createElement("section");
     socials.classList.add(flex, 'socials');
 
     for (btn of [
-        { src: 'linkedin.svg', alt: 'Linkedin link', href: 'https://www.linkedin.com/in/philip-walsh-01' },
-        { src: 'github.svg', alt: 'Github link', href: 'https://github.com/Philip-Walsh' }
+        { src: 'linkedin', alt: 'Linkedin link', href: 'https://www.linkedin.com/in/philip-walsh-01' },
+        { src: 'github', alt: 'Github link', href: 'https://github.com/Philip-Walsh' }
     ]) {
-        let img = getImage(btn.src, btn.alt);
-        img.addEventListener('click', function (event) {
+        socials.prependSvg(btn.src, btn.alt, 'click', function (event) {
             event.preventDefault();
             window.location.href = btn.href;
-        })
-        socials.appendChild(img);
+        });;
     }
     socials.appendChild(getImage('', ''))
     footer.appendChild(socials);
@@ -53,11 +75,10 @@ function addFooter() {
 }
 
 
-document.querySelector('.card')
+document.querySelector('main')
     .addEventListener('mouseover', function hover(event) {
         addHeader();
         addFooter();
-        event
-            .currentTarget
+        event.currentTarget
             .removeEventListener('mouseover', hover);
     });
